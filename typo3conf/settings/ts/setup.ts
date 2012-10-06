40f37377c784c2a6d65e2be05e6ac3aaf4936dca
Visay Keo <keo@visay.info>
2012-10-06 11:22:26 +0700

    [TASK] Adjust styles

--- typo3conf/settings/ts/setup.ts
+++ typo3conf/settings/ts/setup.ts
@@ -72,33 +72,34 @@ page.headerData.10 {
   # Include LESS
 page.headerData.20 = TEXT
 page.headerData.20.value (
-  <link href="fileadmin/templates/bootstrap/less/bootstrap.less" rel="stylesheet/less" />
+	<link href='http://fonts.googleapis.com/css?family=Nova+Slim|Open+Sans' rel='stylesheet' type='text/css'>
+	<link href="fileadmin/templates/bootstrap/less/bootstrap.less" rel="stylesheet/less" />
 
-  <style type="text/css">
-    body {
-      padding-top: 60px;
-      padding-bottom: 40px;
-    }
-    .sidebar-nav {
-      padding: 9px 0;
-    }
-  </style>
+	<style type="text/css">
+		body {
+			padding-top: 60px;
+			padding-bottom: 40px;
+		}
+		.sidebar-nav {
+			padding: 9px 0;
+		}
+	</style>
 
-  <link href="fileadmin/templates/bootstrap/less/responsive.less" rel="stylesheet/less" />
-  <link href="fileadmin/templates/styles/visay-info.less" rel="stylesheet/less" />
-  <link href="fileadmin/templates/styles/prettify.css" rel="stylesheet" />
+	<link href="fileadmin/templates/bootstrap/less/responsive.less" rel="stylesheet/less" />
+	<link href="fileadmin/templates/styles/visay-info.less" rel="stylesheet/less" />
+	<link href="fileadmin/templates/styles/prettify.css" rel="stylesheet" />
 
-  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-  <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 
-  <!-- fav and touch icons -->
-  <link rel="shortcut icon" href="fileadmin/templates/images/icons/favicon.ico" />
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="fileadmin/templates/images/icons/apple-touch-icon-144-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="fileadmin/templates/images/icons/apple-touch-icon-114-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="fileadmin/templates/images/icons/apple-touch-icon-72-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" href="fileadmin/templates/images/icons/apple-touch-icon-57-precomposed.png" />
+	<!-- fav and touch icons -->
+	<link rel="shortcut icon" href="fileadmin/templates/images/icons/favicon.ico" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="fileadmin/templates/images/icons/apple-touch-icon-144-precomposed.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="fileadmin/templates/images/icons/apple-touch-icon-114-precomposed.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="fileadmin/templates/images/icons/apple-touch-icon-72-precomposed.png" />
+	<link rel="apple-touch-icon-precomposed" href="fileadmin/templates/images/icons/apple-touch-icon-57-precomposed.png" />
 )
 
 # Description
