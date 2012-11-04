1137a2937502e512565da50f0403d6f8e0b38a88
Visay Keo <keo@visay.info>
2012-11-04 20:52:50 +0700

    [TASK] Use visitor email for sender email to admin

--- typo3conf/settings/ts/plugin/formhandler/constants.ts
+++ typo3conf/settings/ts/plugin/formhandler/constants.ts
@@ -13,7 +13,7 @@ formhandler.basic.contact-form {
 		admin {
 
 			# cat=Formhandler - Basic - Contact Form/basic/20; type=string; label=Admin email sender: Email address to use as sender address for the admin email.
-			sender_email = {$constant.sender_email}
+			sender_email = email
 
 			# cat=Formhandler - Basic - Contact Form/basic/20; type=string; label=Admin email recipient: Email address of an admin to receive the contact request.
 			to_email = {$constant.to_email}
